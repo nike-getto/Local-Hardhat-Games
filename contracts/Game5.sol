82efe5cb909fb3b092c9b59999afc82d98d4b754
nike-getto <savastanisic7@gmail.com>
2023-02-23 11:43:56 +0100

    Another assignement and the solution to it

--- contracts/Game5.sol
+++ contracts/Game5.sol
@@ -2,22 +2,22 @@
 pragma solidity ^0.8.17;
 
 contract Game5 {
-  mapping(address => uint) balances;
-  mapping(address => uint) allowances;
+    mapping(address => uint) balances;
+    mapping(address => uint) allowances;
 
-  function giveMeAllowance(uint allowance) external {
-    allowances[msg.sender] += allowance;
-  }
+    function giveMeAllowance(uint allowance) external {
+        allowances[msg.sender] += allowance;
+    }
 
-  function mint(uint amount) external {
-    allowances[msg.sender] -= amount;
-    balances[msg.sender] += amount;
-  }
+    function mint(uint amount) external {
+        allowances[msg.sender] -= amount;
+        balances[msg.sender] += amount;
+    }
 
-  event Winner(address winner);
+    event Winner(address winner);
 
-  function win() public {
-    require(balances[msg.sender] >= 10000);
-    emit Winner(msg.sender);
-  }
+    function win() public {
+        require(balances[msg.sender] >= 10000);
+        emit Winner(msg.sender);
+    }
 }
